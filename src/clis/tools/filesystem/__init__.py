d71535d7532e20f25f8f6f46ed066ef665f51216
Eski Yin <eski.yin@gmail.com>
2026-01-11 05:41:08 +0800

    Add DeleteFileTool to filesystem tools exports
    Imported DeleteFileTool and included it in the __all__ list to make it available as part of the filesystem tools module.

--- src/clis/tools/filesystem/__init__.py
+++ src/clis/tools/filesystem/__init__.py
@@ -2,12 +2,14 @@
 Filesystem tools for CLIS.
 """
 
+from clis.tools.filesystem.delete_file import DeleteFileTool
 from clis.tools.filesystem.search_files import SearchFilesTool
 from clis.tools.filesystem.file_tree import FileTreeTool
 from clis.tools.filesystem.write_file import WriteFileTool
 from clis.tools.filesystem.get_file_info import GetFileInfoTool
 
 __all__ = [
+    "DeleteFileTool",
     "SearchFilesTool",
     "FileTreeTool",
     "WriteFileTool",
