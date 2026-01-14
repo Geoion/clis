a11d38996833235cda45bf3cf4a0b117e53002f6
Eski Yin <eski.yin@gmail.com>
2026-01-15 03:13:27 +0800

    Add new filesystem tools to module exports
    Added CodebaseSearchTool, SearchReplaceTool, FindDefinitionTool, FindReferencesTool, GetSymbolsTool, InsertCodeTool, and DeleteLinesTool to the imports and __all__ list for filesystem tools. This makes these tools available for use throughout the package.

--- src/clis/tools/filesystem/__init__.py
+++ src/clis/tools/filesystem/__init__.py
@@ -10,6 +10,13 @@ from clis.tools.filesystem.search_files import SearchFilesTool
 from clis.tools.filesystem.file_tree import FileTreeTool
 from clis.tools.filesystem.write_file import WriteFileTool
 from clis.tools.filesystem.get_file_info import GetFileInfoTool
+from clis.tools.filesystem.codebase_search import CodebaseSearchTool
+from clis.tools.filesystem.search_replace import SearchReplaceTool
+from clis.tools.filesystem.find_definition import FindDefinitionTool
+from clis.tools.filesystem.find_references import FindReferencesTool
+from clis.tools.filesystem.get_symbols import GetSymbolsTool
+from clis.tools.filesystem.insert_code import InsertCodeTool
+from clis.tools.filesystem.delete_lines import DeleteLinesTool
 
 __all__ = [
     "DeleteFileTool",
@@ -20,4 +27,11 @@ __all__ = [
     "FileTreeTool",
     "WriteFileTool",
     "GetFileInfoTool",
+    "CodebaseSearchTool",
+    "SearchReplaceTool",
+    "FindDefinitionTool",
+    "FindReferencesTool",
+    "GetSymbolsTool",
+    "InsertCodeTool",
+    "DeleteLinesTool",
 ]
