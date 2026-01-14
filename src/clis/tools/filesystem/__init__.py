f42619ba12e7fd925fd1cbe0e6eccdd92e453b3e
Eski Yin <eski.yin@gmail.com>
2026-01-14 13:32:07 +0800

    feat: update src/clis/tools/filesystem/__init__.py

--- src/clis/tools/filesystem/__init__.py
+++ src/clis/tools/filesystem/__init__.py
@@ -3,6 +3,9 @@ Filesystem tools for CLIS.
 """
 
 from clis.tools.filesystem.delete_file import DeleteFileTool
+from clis.tools.filesystem.edit_file import EditFileTool
+from clis.tools.filesystem.grep import GrepTool
+from clis.tools.filesystem.read_lints import ReadLintsTool
 from clis.tools.filesystem.search_files import SearchFilesTool
 from clis.tools.filesystem.file_tree import FileTreeTool
 from clis.tools.filesystem.write_file import WriteFileTool
@@ -10,6 +13,9 @@ from clis.tools.filesystem.get_file_info import GetFileInfoTool
 
 __all__ = [
     "DeleteFileTool",
+    "EditFileTool",
+    "GrepTool",
+    "ReadLintsTool",
     "SearchFilesTool",
     "FileTreeTool",
     "WriteFileTool",
