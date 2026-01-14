fc80608d1632ca81d522c1e3414153babb5b2990
Eski Yin <eski.yin@gmail.com>
2026-01-14 13:32:08 +0800

    feat: update src/clis/tools/system/__init__.py

--- src/clis/tools/system/__init__.py
+++ src/clis/tools/system/__init__.py
@@ -6,10 +6,12 @@ from clis.tools.system.system_info import SystemInfoTool
 from clis.tools.system.check_command import CheckCommandTool
 from clis.tools.system.get_env import GetEnvTool
 from clis.tools.system.list_processes import ListProcessesTool
+from clis.tools.system.run_terminal_cmd import RunTerminalCmdTool
 
 __all__ = [
     "SystemInfoTool",
     "CheckCommandTool",
     "GetEnvTool",
     "ListProcessesTool",
+    "RunTerminalCmdTool",
 ]
